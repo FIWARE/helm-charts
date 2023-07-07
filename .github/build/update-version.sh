1649f4b7510f6905388d8487497334191ac5f574
Stefan Wiedemann <wistefan@googlemail.com>
2023-07-07 11:26:35 +0200

    remove spaces

--- .github/build/update-version.sh
+++ .github/build/update-version.sh
@@ -1,41 +1,34 @@
-#!/bin/bash
+ prType=${{ steps.pr.outputs.labels }}
+          declare -A changedCharts
 
-declare -a changedFiles=("${!1}")
-prType=$2
-declare -A changedCharts
+          for file in ${{ steps.changed-files.outputs.all_changed_and_modified_files }}; do         
 
-echo Type is $prType
-echo Files to be evaluated $changedFiles
+              echo "$file was changed"
+              baseFolder=$(cut -d'/' -f1 <<< "$file")
+              if [ $baseFolder = "charts" ]; then
+                  chartName=$(cut -d'/' -f2 <<< "$file")
+                  changedCharts[$chartName]=$chartName
+              fi  
+          done    
 
-for file in ${changedFiles[@]}; do         
+          for c in "${changedCharts[@]}"; do
+              # get version from chart yaml
+              version=$(yq e '.version' "charts/$c/Chart.yaml")
+              major=$(cut -d'.' -f1 <<< "$version")
+              minor=$(cut -d'.' -f2 <<< "$version")
+              patch=$(cut -d'.' -f3 <<< "$version")
 
-    echo "$file was changed"
-    baseFolder=$(cut -d'/' -f1 <<< "$file")
-    if [ $baseFolder = "charts" ]; then
-        chartName=$(cut -d'/' -f2 <<< "$file")
-        changedCharts[$chartName]=$chartName
-    fi  
-done    
-
-for c in "${changedCharts[@]}"; do
-    # get version from chart yaml
-    version=$(yq e '.version' "charts/$c/Chart.yaml")
-    major=$(cut -d'.' -f1 <<< "$version")
-    minor=$(cut -d'.' -f2 <<< "$version")
-    patch=$(cut -d'.' -f3 <<< "$version")
-    if [ $prType = "major" ]; then 
-        major=$((major+1))
-        minor=0
-        patch=0
-    elif [ $prType = "minor" ]; then
-        minor=$((minor+1))
-        patch=0
-    elif [ $prType = "patch" ]; then 
-        patch=$((patch+1))
-    fi
-
-    yq e -i '.version = "'$major.$minor.$patch'"' charts/$c/Chart.yaml
-done
-
-
-
+              echo $prType
+              if [ $prType="major" ]; then 
+                  major=$((major+1))
+                  minor=0
+                  patch=0
+              elif [ $prType="minor" ]; then
+                  minor=$((minor+1))
+                  patch=0
+              elif [ $prType="patch" ]; then 
+                  patch=$((patch+1))
+              fi
+              echo Update version to $major.$minor.$patch for $c
+              yq e -i '.version = "'$major.$minor.$patch'"' charts/$c/Chart.yaml
+          done
